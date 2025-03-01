f9554944068eec5028fdee1fcf1e4501f29e9f5c
haarrsshhhh03 <harshkadhane97@gmail.com>
2025-03-01 11:34:36 +0530

    created a new method for buildprompt , now creating for craft request

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,4 +1,28 @@
 package com.email.writer.app;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailGeneratorService {
+    public String generateEmailREply(EmailRequest emailRequest){
+
+        //Building the promt
+        String prompt =buildprompt(emailRequest);
+
+        //Craft a request ---> folow the json like format
+
+        //do request and get response
+
+    }
+
+    private String buildprompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("generate a proff email reply to the following mail. please dont reply");
+        if(emailRequest.getTone()!= null && !emailRequest.getTone().isEmpty()){
+            prompt.append("use a ").append(emailRequest.getTone()).append("tone.");
+        }
+        prompt.append("\n Original email: \n").append(emailRequest.getEmailContent());
+
+        return prompt.toString();
+    }
 }
